7ec248da240cf0e91634df977638a99d84ba1e14
liu <2369416824@qq.com>
2023-10-06 00:04:34 +0800

    optimize base.lua autocmd and p-comment.lua

--- lua/base.lua
+++ lua/base.lua
@@ -32,27 +32,30 @@ vim.opt.smartcase = true   -- smart case
 vim.opt.smartindent = true -- make indenting smarter again
 vim.opt.ignorecase = true
 
-vim.opt.splitbelow = true                  -- force all horizontal splits to go below current window
-vim.opt.splitright = true                  -- force all vertical splits to go to the right of current window
-vim.opt.swapfile = false                   -- creates a swapfile
-vim.opt.termguicolors = true               -- set term gui colors (most terminals support this)
+vim.opt.splitbelow = true    -- force all horizontal splits to go below current window
+vim.opt.splitright = true    -- force all vertical splits to go to the right of current window
+vim.opt.swapfile = false     -- creates a swapfile
+vim.opt.termguicolors = true -- set term gui colors (most terminals support this)
 -- vim.opt.timeoutlen = 100                   -- time to wait for a mapped sequence to complete (in milliseconds)
-vim.opt.title = true                       -- set the title of window to the value of the titlestring
-vim.opt.titlestring = "%<%F%=%l/%L - nvim" -- what the title of the window will be set to
+vim.opt.title = true         -- set the title of window to the value of the titlestring
+
+-- In some terminals, setting title may cause some problems such as garbled messages when sliding screen by mouse wheel or arrow keys.
+-- vim.opt.titlestring = "%<%F%=%l/%L - nvim" -- what the title of the window will be set to
+
 vim.opt.undodir = vim.fn.stdpath "cache" .. "/undo"
-vim.opt.undofile = true                    -- enable persistent undo
-vim.opt.updatetime = 300                   -- faster completion
-vim.opt.writebackup = false                -- if a file is being edited by another program (or was written to file while editing with another program) it is not allowed to be edited
-vim.opt.expandtab = true                   -- convert tabs to spaces
-vim.opt.shiftwidth = 2                     -- the number of spaces inserted for each indentation
-vim.opt.tabstop = 2                        -- insert 2 spaces for a tab
-vim.opt.cursorline = true                  -- highlight the current line
-vim.opt.number = true                      -- set numbered lines
-vim.opt.relativenumber = true              -- set relative numbered lines
-vim.opt.numberwidth = 2                    -- set number column width to 2 {default 4}
-vim.opt.signcolumn = "yes"                 -- always show the sign column otherwise it would shift the text each time
-vim.opt.wrap = false                       -- display lines as one long line
-vim.opt.scrolloff = 4                      -- is one of my fav
+vim.opt.undofile = true       -- enable persistent undo
+vim.opt.updatetime = 300      -- faster completion
+vim.opt.writebackup = false   -- if a file is being edited by another program (or was written to file while editing with another program) it is not allowed to be edited
+vim.opt.expandtab = true      -- convert tabs to spaces
+vim.opt.shiftwidth = 2        -- the number of spaces inserted for each indentation
+vim.opt.tabstop = 2           -- insert 2 spaces for a tab
+vim.opt.cursorline = true     -- highlight the current line
+vim.opt.number = true         -- set numbered lines
+vim.opt.relativenumber = true -- set relative numbered lines
+vim.opt.numberwidth = 2       -- set number column width to 2 {default 4}
+vim.opt.signcolumn = "yes"    -- always show the sign column otherwise it would shift the text each time
+vim.opt.wrap = false          -- display lines as one long line
+vim.opt.scrolloff = 4         -- is one of my fav
 vim.opt.sidescrolloff = 8
 
 -- vim.cmd.colorscheme "catppuccin-macchiato"
@@ -62,12 +65,16 @@ vim.o.timeoutlen = 500
 
 
 -- make .zshrc highlight like bash file
-vim.api.nvim_create_autocmd({ "BufEnter", "BufWinEnter" }, {
-  pattern = { ".zshrc" },
-  callback = function()
-    require("nvim-treesitter.highlight").attach(0, "bash")
-  end
-})
+local nvim_treesitter_status_ok = pcall(require, "nvim-treesitter.highlight")
+
+if nvim_treesitter_status_ok then
+  vim.api.nvim_create_autocmd({ "BufEnter", "BufWinEnter" }, {
+    pattern = { ".zshrc" },
+    callback = function()
+      require("nvim-treesitter.highlight").attach(0, "bash")
+    end
+  })
+end
 
 
 -- I like 4 spaces indent
@@ -81,16 +88,19 @@ augroup highlight_yank
 augroup END
 ]]
 
-
+-- Highlight the variable under cursor
 vim.cmd [[
 set signcolumn=yes
 autocmd CursorHold * lua vim.lsp.buf.document_highlight()
 autocmd CursorMoved * lua vim.lsp.buf.clear_references()
 ]]
 
+
 -- autocmd created
-vim.api.nvim_create_autocmd({ "BufEnter" }, {
-  callback = function()
-    vim.lsp.inlay_hint(0, true)
-  end
-})
+if vim.lsp.inlay_hint then
+  vim.api.nvim_create_autocmd({ "BufEnter" }, {
+    callback = function()
+      vim.lsp.inlay_hint(0, true)
+    end
+  })
+end
--- lua/p-comment.lua
+++ lua/p-comment.lua
@@ -1,1 +1,11 @@
-require('Comment').setup()
+require('Comment').setup {
+  mappings = {
+    ---Operator-pending mapping; `gcc` `gbc` `gc[count]{motion}` `gb[count]{motion}`
+    basic = false,
+    ---Extra mapping; `gco`, `gcO`, `gcA`
+    extra = false,
+  },
+}
+vim.keymap.set("n", "<leader>/", "<cmd>lua require('Comment.api').toggle.linewise.current()<CR>", my_opts)
+vim.keymap.set("x", "<leader>/", "<esc><cmd>lua require('Comment.api').toggle.linewise(vim.fn.visualmode())<CR>", my_opts)
+
