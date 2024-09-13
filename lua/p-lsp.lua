a0e44bb5d8c3ebaa875327e9dce779ddafff4b49
dtfulffroot <2369416824@qq.com>
2024-09-13 21:06:50 +0800

    normal update

--- lua/p-lsp.lua
+++ lua/p-lsp.lua
@@ -7,7 +7,7 @@ require("mason-lspconfig").setup()
 -- Setup language servers.
 local lspconfig = require('lspconfig')
 lspconfig.pyright.setup {}
-lspconfig.tsserver.setup {}
+lspconfig.ts_ls.setup {}
 lspconfig.rust_analyzer.setup {
     -- Server-specific settings. See `:help lspconfig-setup`
     settings = {
